d98eb5de063ba53e5cf2d0198020b7648c5cd979
alok-shete <shetealok@gmail.com>
2020-06-18 14:58:15 +0530

    Updates

---
+++ precache-manifest.5fe84aef6ac3d112bf63f0b014e692f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f08b685d440b1153254c5c00ed1776d2",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "0e7f0369a3c916825abd",
+    "url": "/calculator/static/css/main.6ec974f5.chunk.css"
+  },
+  {
+    "revision": "83c708f55d771b07501a",
+    "url": "/calculator/static/js/2.028d59cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.028d59cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e7f0369a3c916825abd",
+    "url": "/calculator/static/js/main.f082ba7e.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
